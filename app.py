fdf5db2005d4a22198f5dad5337bd7e9903940dc
Jeet Patel <72094212+Jeet-51@users.noreply.github.com>
2024-04-23 17:50:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -330,6 +330,7 @@ def login():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
+ app.run(host='0.0.0.0', port=8000)
     with app.app_context():
         db.create_all()
     app.run(debug=True)
